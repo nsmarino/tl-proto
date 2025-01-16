918437e868f1bd55d8c1c57703a0fd7e50822b27
Nick Marino <nicholas.marino@zero.nyc>
2025-01-16 14:45:51 -0500

    Use live site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,8 +70,8 @@
 
     for (let i=0;i<flipbookLength;i++) { 
       
-      const staticSrcDesktop = `https://touchland-clp-git-image-optimize-zero-studios.vercel.app/products/${product.id}/flipbook/desktop/${product.filePrefix}${i< 10 ? "0"+i : i}.png`
-      const staticSrcMobile = `https://touchland-clp-git-image-optimize-zero-studios.vercel.app/${product.id}/flipbook/mobile/${product.filePrefix}${i< 10 ? "0"+i : i}.png`
+      const staticSrcDesktop = `https://moveyourmood.touchland.com/products/${product.id}/flipbook/desktop/${product.filePrefix}${i< 10 ? "0"+i : i}.png`
+      const staticSrcMobile = `https://moveyourmood.touchland.com/${product.id}/flipbook/mobile/${product.filePrefix}${i< 10 ? "0"+i : i}.png`
 
       const vercelSrcDesktop = `/_vercel/image?url=${encodeURIComponent(staticSrcDesktop)}&w=1920&q=80`
       const vercelSrcMobile = `/_vercel/image?url=${encodeURIComponent(staticSrcMobile)}&w=800&q=80`
